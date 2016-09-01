551551fe51094952449d8559ec86522f41ed744e
Venryx <venryx@gmail.com>
2016-09-01 14:15:23 -0700

    * ClassName prop is usable again.

--- src/react-treeview.jsx
+++ src/react-treeview.jsx
@@ -1,6 +1,14 @@
 ﻿import React, {PropTypes} from 'react';
 
 class TreeView extends BaseComponent {
+	static propTypes = {
+		collapsed: PropTypes.bool,
+		defaultCollapsed: PropTypes.bool,
+		nodeLabel: PropTypes.node.isRequired,
+		className: PropTypes.string,
+		itemClassName: PropTypes.string
+	};
+
 	constructor(props) {
 	    super(props);
 	    this._bind("onArrowClick", "onClick");
@@ -17,32 +25,23 @@ class TreeView extends BaseComponent {
 	}
 
 	render() {
-		const {
-			collapsed = this.state.collapsed,
-			//className = '',
-			itemClassName = '',
-			nodeLabel,
-			children,
-			//...rest,
-		} = this.props;
+		//var {collapsed = this.state.collapsed, className, itemClassName, nodeLabel, children, selected, style, ...rest} = this.props;
+		var {collapsed = this.state.collapsed, className, itemClassName, nodeLabel, children, selected, style} = this.props;
 
-		//let arrowClassName = 'tree-view_arrow';
 		let containerClassName = 'tree-view_children';
-		if (collapsed) {
-			//arrowClassName += ' tree-view_arrow-collapsed';
+		if (collapsed)
 			containerClassName += ' tree-view_children-collapsed';
-		}
-
+		
 		return (
-			<div className="tree-view">
+			<div className={"tree-view " + className} style={style}>
 				<div onClick={this.onArrowClick}
 					style={{
-						cursor: "pointer", display: "inline-block", width: 8, height: 8, verticalAlign: "top", marginTop: 2, padding: 2,
-						userSelect: "none", WebkitUserSelect: "none", backgroundPosition: 2, backgroundRepeat: "no-repeat", backgroundSize: 8,
-						backgroundImage: children.length ? ("url(/Main/Packages/Images/Buttons/" + (collapsed ? "Right" : "Down") + ".png)") : ""
+						display: "inline-block", boxSizing: "content-box", width: 8, height: 8, verticalAlign: "top", marginTop: 2, padding: 2,
+						userSelect: "none", WebkitUserSelect: "none", backgroundPosition: 2, backgroundRepeat: "no-repeat", backgroundSize: 8, cursor: "pointer",
+						backgroundImage: children && children.length != 0 && ("url(/Main/Packages/Images/Buttons/" + (collapsed ? "Right" : "Down") + ".png)")
 					}}/>
 				<div className={"tree-view_item " + itemClassName} onClick={this.onClick}
-					style={{display: "inline-block", backgroundColor: this.props.selected ? "rgba(44, 79, 122, .5)" : null}}>
+					style={{display: "inline-block", backgroundColor: selected ? "rgba(44, 79, 122, .5)" : null}}>
 					{nodeLabel}
 				</div>
 				<div className={containerClassName}>
@@ -52,12 +51,5 @@ class TreeView extends BaseComponent {
 		);
 	}
 }
-TreeView.propTypes = {
-	collapsed: PropTypes.bool,
-	defaultCollapsed: PropTypes.bool,
-	nodeLabel: PropTypes.node.isRequired,
-	className: PropTypes.string,
-	itemClassName: PropTypes.string,
-};
 
 export default TreeView;
